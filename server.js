67f5f9a212a48e8b0d0dce4ef29b500b193b9547
Cody Miller <cmiller@catalystitservices.com>
2016-04-29 10:49:17 -0700

    added update

--- server.js
+++ server.js
@@ -41,7 +41,44 @@ app.delete('/todos/:id', function(req, res){
         res.status(200).send(matchedTodo);
     }
 
+});
+
+app.put('/todos/:id', function(req, res){
+    var todoId = parseInt(req.params.id);
+    var body = req.body;
+    updateTodo(body, todoId).then(function(success){
+        res.json(success);
+    }, function(err){
+        res.status(500).send(err.errorMessage);
+    });
 })
+
+function updateTodo(todo, id){
+    return new Promise(function(resolve, reject){
+        var newTodo = _.pick(todo, 'completed', 'description');
+        var matchedTodo = _.findWhere(todos, {id: id});
+        var validAttributes = {};
+
+        if(!matchedTodo){
+            reject({errorMessage: 'No todo with id' + id});
+        }
+
+        if(newTodo.hasOwnProperty('completed') && _.isBoolean(todo.completed)){
+            validAttributes.completed = todo.completed;
+        } else if (newTodo.hasOwnProperty('completed')){
+            reject({errorMessage: 'Completed is not a boolean'});
+        }
+
+        if(newTodo.hasOwnProperty('description') && _.isString(todo.description) && newTodo.description.trim().length > 0){
+            validAttributes.description = todo.description;
+        } else if (newTodo.hasOwnProperty('description')){
+            reject({errorMessage: 'Description is not a string'});
+        }
+        resolve(_.extend(matchedTodo, validAttributes));
+
+    })
+}
+
 
 function addTodo(todo){
     return new Promise(function(resolve, reject){
