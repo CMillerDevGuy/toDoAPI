a212f659a45e639e6e0cd1e4e1866f73981e3044
Cody Miller <cmiller@catalystitservices.com>
2016-05-02 13:55:05 -0700

    added put with db

--- server.js
+++ server.js
@@ -79,38 +79,27 @@ app.delete('/todos/:id', function(req, res){
 app.put('/todos/:id', function(req, res){
     var todoId = parseInt(req.params.id);
     var body = req.body;
-    updateTodo(body, todoId).then(function(success){
-        res.json(success);
-    }, function(err){
-        res.status(500).send(err.errorMessage);
+    var attributes = {};
+
+    if(body.hasOwnProperty('completed')){
+        attributes.completed = body.completed;
+    }
+    if(body.hasOwnProperty('description')){
+        attributes.description = body.description;
+    }
+
+    db.todo.findById(todoId).then(function(todo){
+        if(todo){
+            return todo.update(attributes);
+        }else{
+            res.status(404).send('No Todo Found');
+        }
+    }).then(function(todo){
+        res.json(todo.toJSON());
+    }).catch(function(err){
+       res.status(500).json(err);
     });
-})
-
-function updateTodo(todo, id){
-    return new Promise(function(resolve, reject){
-        var newTodo = _.pick(todo, 'completed', 'description');
-        var matchedTodo = _.findWhere(todos, {id: id});
-        var validAttributes = {};
-
-        if(!matchedTodo){
-            reject({errorMessage: 'No todo with id' + id});
-        }
-
-        if(newTodo.hasOwnProperty('completed') && _.isBoolean(todo.completed)){
-            validAttributes.completed = todo.completed;
-        } else if (newTodo.hasOwnProperty('completed')){
-            reject({errorMessage: 'Completed is not a boolean'});
-        }
-
-        if(newTodo.hasOwnProperty('description') && _.isString(todo.description) && newTodo.description.trim().length > 0){
-            validAttributes.description = todo.description;
-        } else if (newTodo.hasOwnProperty('description')){
-            reject({errorMessage: 'Description is not a string'});
-        }
-        resolve(_.extend(matchedTodo, validAttributes));
-
-    })
-}
+});
 
 app.get('/', function(req, res){
     res.send('Todo API root');
